ee68a3682b425178d317bc1dc62004f1ef0a8074
tulagarns <tulagarn.s@gmail.com>
2025-03-21 19:33:53 +0700

    Add login Add Register Add React-Router Add CreateUser

--- flask-backend/dataModel.py
+++ flask-backend/dataModel.py
@@ -3,14 +3,16 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-class User(db.Model):
+#Snake case = Python / Camel case = JSON
+
+class User(db.Model): #To indicate that this is a database model
     __tablename__ = 'User'
     __table_args__ = {'schema': 'SP2024-4'}
 
     UserID = db.Column(db.String(50), primary_key=True)
     UserFirstName = db.Column(db.String(50), nullable=False)
     UserLastName = db.Column(db.String(50), nullable=False)
-    UserPassword = db.Column(db.String(100), nullable=False)  # Should be hashed
+    UserPassword = db.Column(db.String(100), nullable=False)
     UserEmail = db.Column(db.String(100), unique=True, nullable=False)
     UserPicURL = db.Column(db.String(100))
     AccountType = db.Column(db.Integer, nullable=False)
@@ -18,3 +20,10 @@ class User(db.Model):
     Company = db.Column(db.String(2000))
     CreatedOn = db.Column(db.DateTime, default=datetime.utcnow)
     IsActive = db.Column(db.Boolean, default=True)
+
+    def to_json(self):
+        return {
+            'userID': self.UserID,
+            'userFirstName': self.UserFirstName,
+            'userLastName': self.UserLastName,
+        }
