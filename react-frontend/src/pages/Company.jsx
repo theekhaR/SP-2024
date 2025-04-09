25e089a872413d7a86339ebd0734d6cb3cbc5b54
TheekhaR <boon1131@gmail.com>
2025-04-09 23:49:16 +0700

    modified:   react-frontend/src/pages/Company.jsx

--- react-frontend/src/pages/Company.jsx
+++ react-frontend/src/pages/Company.jsx
@@ -43,7 +43,7 @@ function Company() {
       <Lnavbar />
       <div className="max-w-7xl mx-auto py-10 px-4">
         <div className="flex justify-between items-center mb-6">
-          <h2 className="text-2xl font-semibold">
+          <h2 className="text-2xl font-bold">
             My Company ({companies.length})
           </h2>
           <button className="bg-orange-600 px-4 py-2 rounded hover:bg-orange-600">
@@ -69,7 +69,7 @@ function Company() {
                     <img
                       src={company.image_url}
                       alt={company.name}
-                      className="h-12 w-12 object-contain"
+                      className="font-semibold h-12 w-12 object-contain"
                     />
                     <span>{company.name}</span>
                   </td>
@@ -86,7 +86,7 @@ function Company() {
                       className={`px-4 py-1 rounded ${
                         company.role === "Administrator"
                           ? "bg-orange-600 text-white hover:bg-orange-700"
-                          : "bg-orange-200 text-white cursor-not-allowed"
+                          : "bg-[#F9AD95] text-white cursor-not-allowed"
                       }`}
                       disabled={company.role !== "Administrator"}
                     >
